0cc3d1fbc676f6e201bbb945b5343f0887ae86e0
Angelica <Angelica137@users.noreply.github.com>
2022-07-25 20:16:14 +0100

    translate a long strand, green

--- scripts/rnatranscript.py
+++ scripts/rnatranscript.py
@@ -1,6 +1,9 @@
 def rna_transcript(strand: str) -> str:
     translations = {'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
-    for key in translations:
-        if key in strand:
-            return translations[key]
-    raise ValueError('DNA strand incorrect')
+    rna_strand = ""
+    for char in strand:
+        if char in translations:
+            rna_strand += translations[char]
+        else:
+            raise ValueError('DNA strand incorrect')
+    return rna_strand
