127e5a64d272df50464248b07933186201acc767
Angelica <Angelica137@users.noreply.github.com>
2022-07-25 19:40:13 +0100

    rna_transcript() correctly transcribes T, red

--- tests/test_rnatranscript.py
+++ tests/test_rnatranscript.py
@@ -7,3 +7,7 @@ def test_rna_transcript_c():
 
 def test_rns_transcript_returns_G():
     assert rna_transcript('C') == 'G'
+
+
+def test_rns_transcript_returns_A():
+    assert rna_transcript('T') == 'A'
