eb966c465c370d49f9a3ac54674f7a76539a4969
Angelica <Angelica137@users.noreply.github.com>
2022-07-28 19:56:16 +0100

    refactor

--- scripts/rnatranscript.py
+++ scripts/rnatranscript.py
@@ -1,9 +1,7 @@
 def rna_transcript(strand: str) -> str:
     translations = {'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
-    rna_strand = ""
-    for char in strand:
-        if char in translations:
-            rna_strand += translations[char]
-        else:
-            raise ValueError('DNA strand incorrect')
-    return rna_strand
+    rna_strand = "".join(map(lambda k: translations.get(k, ''), strand))
+    if len(rna_strand) == len(strand):
+        return rna_strand
+    else:
+        raise ValueError('DNA strand incorrect')
